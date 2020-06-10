9e168a0049edd33f9d67926b5b1ce8d1505b9d9c
Maxym Berezynets <halustyan@ukr.net>
2020-06-10 15:26:04 +0300

    add all

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+'use strict';
 var arr = [];
 
 var map = document.querySelector('.map');
@@ -5,59 +6,59 @@ var map = document.querySelector('.map');
 map.classList.remove('map--faded');
 
 function randomInteger(min, max) {
-  let rand = min - 0.5 + Math.random() * (max - min + 1);
+  var rand = min - 0.5 + Math.random() * (max - min + 1);
   return Math.round(rand);
 }
 
-var getRandomArrayEl = function (arr) {
-  var rand = Math.floor(Math.random() * arr.length);
-  return arr[rand];
+var getRandomArrayEl = function (randarr) {
+  var rand = Math.floor(Math.random() * randarr.length);
+  return randarr[rand];
 };
 
 function createObject(index) {
   var sizeofthepin = 40;
 
-  var getmappinswidth = document.querySelector(".map__pins").offsetWidth;
+  var getmappinswidth = document.querySelector('.map__pins').offsetWidth;
 
   var minHeight = 130;
 
   var maxHeight = 630;
 
-  var titles = ["Title1", "Title2", "Title3", "Title4", "Title5", "Title6", "Title7", "Title8"];
+  var titles = ['Title1', 'Title2', 'Title3', 'Title4', 'Title5', 'Title6', 'Title7', 'Title8'];
 
-  var types = ["palace", "flat", "house", "bungalo"];
+  var types = ['palace', 'flat', 'house', 'bungalo'];
 
-  var timerange = ["12:00", "13:00", "14:00"];
+  var timerange = ['12:00', '13:00', '14:00'];
 
-  var features = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
+  var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 
-  var photos = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
+  var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
   var deskriptions = ['cozy place', 'beutiful view', 'modern design'];
 
   var obj = {
 
-    "author": {
-      "avatar": "img/avatars/user0" + (index + 1) + ".png",
+    'author': {
+      'avatar': 'img/avatars/user0' + (index + 1) + '.png',
     },
 
-    "offer": {
-      "title": titles[randomInteger(0, titles.length - 1)],
-      "address": randomInteger(1, 1000) + ', ' + randomInteger(1, 1000),
-      "price": randomInteger(1, 10000),
-      "type": types[randomInteger(0, types.length - 1)],
-      "rooms": randomInteger(1, 3),
-      "guests": randomInteger(1, 6),
-      "checkin": timerange[randomInteger(0, timerange.length - 1)],
-      "checkout": timerange[randomInteger(0, timerange.length - 1)],
-      "features": getRandomArrayEl(features),
-      "description": getRandomArrayEl(deskriptions),
-      "photos": getRandomArrayEl(photos),
+    'offer': {
+      'title': titles[randomInteger(0, titles.length - 1)],
+      'address': randomInteger(1, 1000) + ', ' + randomInteger(1, 1000),
+      'price': randomInteger(1, 10000),
+      'type': types[randomInteger(0, types.length - 1)],
+      'rooms': randomInteger(1, 3),
+      'guests': randomInteger(1, 6),
+      'checkin': timerange[randomInteger(0, timerange.length - 1)],
+      'checkout': timerange[randomInteger(0, timerange.length - 1)],
+      'features': getRandomArrayEl(features),
+      'description': getRandomArrayEl(deskriptions),
+      'photos': getRandomArrayEl(photos),
     },
 
-    "location": {
-      "x": randomInteger(minHeight, maxHeight),
-      "y": randomInteger(sizeofthepin, getmappinswidth - sizeofthepin),
+    'location': {
+      'x': randomInteger(minHeight, maxHeight),
+      'y': randomInteger(sizeofthepin, getmappinswidth - sizeofthepin),
     }
 
   };
@@ -68,25 +69,23 @@ for (var i = 0; i < 8; i++) {
   arr.push(createObject(i));
 }
 
-var mockObjectArray = arr;
+var ElObjectArray = arr;
 
 var pinField = document.querySelector('.map__pins');
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 var fragment = document.createDocumentFragment();
 
-
-var createPinElement = function (itemObj) {
+var createPinElement = function (itemarrayObjEl) {
   var cloneElement = pinTemplate.cloneNode(true);
-  var pinElementImg = cloneElement.querySelector("img");
-  pinElementImg.src = itemObj.author.avatar;
-  cloneElement.style.top = itemObj.location.x + "px";
-  cloneElement.style.left = itemObj.location.y + "px";
+  var pinElementImg = cloneElement.querySelector('img');
+  cloneElement.style.top = itemarrayObjEl.location.x + 'px';
+  cloneElement.style.left = itemarrayObjEl.location.y + 'px';
+  pinElementImg.src = itemarrayObjEl.author.avatar;
   return cloneElement;
-}
+};
 
-
-for (var i = 0; i < mockObjectArray.length; i++) {
-  fragment.appendChild(createPinElement(mockObjectArray[i]));
+for (var b = 0; b < ElObjectArray.length; b++) {
+  fragment.appendChild(createPinElement(ElObjectArray[b]));
   pinField.appendChild(fragment);
 }
 
