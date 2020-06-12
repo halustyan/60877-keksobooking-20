940fd9ebff030abbaf525205f9b72bc0ad56a6ef
Maxym Berezynets <halustyan@ukr.net>
2020-06-12 10:35:10 +0300

    сделал задание task3

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 'use strict';
 var arr = [];
-
 var map = document.querySelector('.map');
 
 map.classList.remove('map--faded');
@@ -15,7 +14,9 @@ var getRandomArrayEl = function (randarr) {
   return randarr[rand];
 };
 
-function createObject(index) {
+var types = ['palace', 'flat', 'house', 'bungalo'];
+
+var createObject = function (index) {
   var sizeofthepin = 40;
 
   var getmappinswidth = document.querySelector('.map__pins').offsetWidth;
@@ -25,8 +26,6 @@ function createObject(index) {
   var maxHeight = 630;
 
   var titles = ['Title1', 'Title2', 'Title3', 'Title4', 'Title5', 'Title6', 'Title7', 'Title8'];
-
-  var types = ['palace', 'flat', 'house', 'bungalo'];
 
   var timerange = ['12:00', '13:00', '14:00'];
 
@@ -69,6 +68,7 @@ for (var i = 0; i < 8; i++) {
   arr.push(createObject(i));
 }
 
+
 var ElObjectArray = arr;
 
 var pinField = document.querySelector('.map__pins');
@@ -81,6 +81,7 @@ var createPinElement = function (itemarrayObjEl) {
   cloneElement.style.top = itemarrayObjEl.location.x + 'px';
   cloneElement.style.left = itemarrayObjEl.location.y + 'px';
   pinElementImg.src = itemarrayObjEl.author.avatar;
+  pinElementImg.alt = itemarrayObjEl.offer.description;
   return cloneElement;
 };
 
@@ -89,4 +90,91 @@ for (var b = 0; b < ElObjectArray.length; b++) {
   pinField.appendChild(fragment);
 }
 
+var Arrayrenderad = arr;
 
+var cardObject = Arrayrenderad[0];
+
+var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
+
+var newCardElement = cardTemplate.cloneNode(true);
+
+var fragmentCard = document.createDocumentFragment();
+fragmentCard.appendChild(newCardElement);
+
+map.appendChild(fragmentCard);
+
+var photoElement = document.querySelector('.popup__photos');
+
+var photoArray = cardObject.offer.photos;
+
+var photoTemplate = document.querySelector('#card')
+  .content
+  .querySelector('.popup__photo');
+
+photoElement.innerHTML = '';
+
+var fragmentPhoto = document.createDocumentFragment();
+
+var renderCard = function (obj) {
+  newCardElement.querySelector('.popup__title').textContent = obj.offer.title;
+  newCardElement.querySelector('.popup__text--address').textContent = obj.offer.address;
+  newCardElement.querySelector('.popup__text--price').textContent = obj.offer.price + '₽/ночь';
+  newCardElement.querySelector('.popup__type').textContent = types[obj.offer.type];
+  newCardElement.querySelector('.popup__text--capacity').textContent = obj.offer.rooms + ' комнаты для ' + obj.offer.guests + ' гостей';
+  newCardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + obj.offer.checkin + ', выезд до ' + obj.offer.checkout;
+  newCardElement.querySelector('.popup__description').textContent = obj.offer.description;
+  newCardElement.querySelector('.popup__features').textContent = obj.offer.features;
+  newCardElement.querySelector('.popup__avatar').src = obj.author.avatar;
+  for (var k = types; k < photoArray.length; k++) {
+    var newPhotoElement = photoTemplate.cloneNode(true);
+    newPhotoElement.src = photoArray[k];
+    fragmentPhoto.appendChild(newPhotoElement);
+  }
+  photoElement.appendChild(fragmentPhoto);
+};
+
+
+
+  renderCard(cardObject);
+
+
+
+
+
+
+
+
+
+/*
+
+var popupTextAddress = document.querySelector('.popup__text--address');
+var popupTextPrice = document.querySelector('.popup__text--price');
+
+var PopupType = document.querySelector('.popup__type');
+
+var createDocumentCardAdd = function (cardel) {
+
+  var ClonePopupTitle =  popupTitle.cloneNode(true);
+
+  var ClonePopupTextAddress =  popupTextAddress.cloneNode(true);
+
+  var ClonePopupTextPrice = popupTextPrice.cloneNode(true);
+
+  var ClonePopupType = PopupType.cloneNode(true);
+
+  if (cardel.offer.type ==='flat') {
+    ClonePopupType.innerHtml = "Квартира";
+  }
+
+  else if (cardel.offer.type ==='bungalo') {
+    ClonePopupType.innerHtml = "Бунгало";
+  }
+
+  ClonePopupTitle.innerHtml = cardel.offer.title;
+
+  ClonePopupTextAddress.innerHtml = cardel.offer.address;
+
+  ClonePopupTextPrice.innerHtml = cardel.offer.price + '₽/ночь';
+
+  return cardel;
+}*/
