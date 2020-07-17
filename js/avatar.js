f1b15a06c53337e7cf9fba537e589b6b41ea4d38
Maxym Berezynets <halustyan@ukr.net>
2020-07-17 22:26:53 +0300

    new

---
+++ js/avatar.js
@@ -0,0 +1,32 @@
+'use strict';
+
+(function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+  var fileChooser = document.querySelector('.ad-form-header__input');
+  var preview = document.querySelector('.ad-form-header__preview img');
+
+  var fileChooserBottom = document.querySelector('.ad-form__upload input');
+  var previewBottom = document.querySelector('.ad-form__photo');
+
+
+  // Первая картинка
+  fileChooser.addEventListener('change', function () {
+    var file = fileChooser.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        preview.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+})();
