fa55a4eee74f8a2aa1f2fa71a9a09ec539999041
Maxym Berezynets <halustyan@ukr.net>
2020-07-27 10:18:35 +0300

    fixes web address

--- js/upload.js
+++ js/upload.js
@@ -2,7 +2,7 @@
 (function () {
 
   window.upload = function (data) {
-    var URL = 'https://javascript.pages.academy/keksobookin';
+    var URL = 'https://javascript.pages.academy/keksobooking';
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
     xhr.addEventListener('load', function () {
